86bc5028a8284305f14573450155e7dd4c04152b
Samuel Martins <samuel.martins4.sm@gmail.com>
2023-01-09 23:09:28 +0300

    feature: add the image filtering functionality

--- src/server.ts
+++ src/server.ts
@@ -14,24 +14,10 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
   app.use(bodyParser.json());
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
   app.get(
     "/filteredimage",
     async (req: Request, res: Response, next: NextFunction) => {
-      let imageUrl: string = req.query.image_url;
+      let imageUrl: string = String(req.query.image_url);
 
       if (!imageUrl) {
         return res.status(400).send({ message: "Image url is required" });
@@ -40,8 +26,9 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
         let filteredImagePath: string = (await filterImageFromURL(
           imageUrl
         )) as string;
-        res.sendFile(filteredImagePath);
-        res.on("finish", () => deleteLocalFiles([filteredImagePath]));
+        res.status(200).sendFile(filteredImagePath, () => {
+          deleteLocalFiles([filteredImagePath]);
+        });
       } catch (err) {
         return next(err);
       }
